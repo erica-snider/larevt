5c93c569ff123c56274c012bc79e06a61189b95b
Erica Snider <erica@fnal.gov>
2024-03-13 15:23:20 -0500

    Tweak method comments to clarify correction convention. Use geo::TPCID instead of int's to specify TPC

--- larevt/SpaceCharge/SpaceCharge.h
+++ larevt/SpaceCharge/SpaceCharge.h
@@ -44,17 +44,24 @@ namespace spacecharge {
     virtual geo::Vector_t GetPosOffsets(geo::Point_t const& point, geo::TPCID const& tpcID) const = 0;
    
     /// Nom Efield vector + mag(Efield) * GetEfieldOffsets(true point) = Actual Efield at true point
-    /// All values are in global coordinates
+    /// All values are in global coordinates, so method is aware of drift direction
+    /// Note that this method must calculate the TPD ID, so is potentially expensive
     virtual geo::Vector_t GetEfieldOffsets(geo::Point_t const& point) const = 0;
     
+    /// Nom Efield vector + mag(Efield) * GetEfieldOffsets(true point) = Actual Efield at true point
+    /// Preferred method when the TPC ID is known
+    /// All values are in global coordinates, so method is aware of drift direction
+    virtual geo::Vector_t GetEfieldOffsets(geo::Point_t const& point, geo::TPCID const& tpcID) const = 0;
+
     /// GetCalPosOffsets(measured point, TPC ID) + measured point = estimated true position
     /// All values are in global coorinates, so method is aware of drift direction for given TPC ID
-    virtual geo::Vector_t GetCalPosOffsets(geo::Point_t const& point, int const& TPCid) const = 0;
+    virtual geo::Vector_t GetCalPosOffsets(geo::Point_t const& point, geo::TPCID const& tpcID) const = 0;
     
     // To get corrected field at measured point, first calculate estimated true position, 
     // then get Efield offsets for that position
+    // Preferred method is to get estimated true position, then call GetEfieldOffsets
     virtual geo::Vector_t GetCalEfieldOffsets(geo::Point_t const& point,
-                                              int const& TPCid) const = 0;
+                                              geo::TPCID const& tpcID) const = 0;
 
   protected:
     SpaceCharge() = default;
